652ecc4011f67f9556eb2febfae28f3931ea62b7
zohaibadnan137 <zohaibadnan137@gmail.com>
2021-06-20 14:38:09 +0500

    Added a cool menu

--- Network Analysis using Graphs/Menu.h
+++ Network Analysis using Graphs/Menu.h
@@ -1,86 +1,153 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-#define MAX_ITEMS 7
+#define MAX_ITEMS 16
 
-class CoolMenu
+class coolMenu
 {
+private:
+	int selectedItemIndex;
+	sf::Font font;
+	sf::Text text[MAX_ITEMS];
+
+	sf::Texture texture;
+	sf::Sprite background;
+
 public:
-	CoolMenu(float width, float height)
+	coolMenu(float width, float height)
 	{
-		if (!font.loadFromFile("./arial.ttf"))
-		{
-			// Handle error
-		}
+		font.loadFromFile("./arial.ttf");
 
-		if (!texture.loadFromFile("./Texture.png"))
-		{
-			// Texture error
-		}
-
+		texture.loadFromFile("./Texture.png");
 		background.setTexture(texture);
 		background.setPosition(0, 150);
 
 		text[0].setFont(font);
 		text[0].setFillColor(sf::Color::Cyan);
 		text[0].setString("Run All Tasks");
-		text[0].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 0.5));
+		text[0].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 1));
 
+		// Task 1
 		text[1].setFont(font);
 		text[1].setFillColor(sf::Color::White);
 		text[1].setString("Number of Nodes");
-		text[1].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 1));
+		text[1].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 2));
 
+		// Task 2
 		text[2].setFont(font);
 		text[2].setFillColor(sf::Color::White);
 		text[2].setString("Number of Edges");
-		text[2].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 1.5));
+		text[2].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 3));
 
+		// Task 3
 		text[3].setFont(font);
 		text[3].setFillColor(sf::Color::White);
 		text[3].setString("Number of Source Nodes");
-		text[3].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 2));
+		text[3].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 4));
 
+		// Task 4
 		text[4].setFont(font);
 		text[4].setFillColor(sf::Color::White);
 		text[4].setString("Number of Sink Nodes");
-		text[4].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 2.5));
+		text[4].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 5));
 
+		// Task 5
 		text[5].setFont(font);
 		text[5].setFillColor(sf::Color::White);
 		text[5].setString("Number of Isolated Nodes");
-		text[5].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 3));
+		text[5].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 6));
 
+		// Task 6
 		text[6].setFont(font);
 		text[6].setFillColor(sf::Color::White);
-		text[6].setString("Number of Articulation Nodes");
-		text[6].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 3.5));
-	}
-	~CoolMenu()
-	{
+		text[6].setString("Number of Bridge Edges");
+		text[6].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 7));
 
+		// Task 7
+		text[7].setFont(font);
+		text[7].setFillColor(sf::Color::White);
+		text[7].setString("Number of Articulation Nodes");
+		text[7].setPosition(sf::Vector2f(width / 15, height / (MAX_ITEMS + 1) * 8));
+
+		// Task 8
+		text[8].setFont(font);
+		text[8].setFillColor(sf::Color::White);
+		text[8].setString("Shortest Path Length Distribution*");
+		text[8].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 1));
+
+		// Task 9
+		text[9].setFont(font);
+		text[9].setFillColor(sf::Color::White);
+		text[9].setString("Diameter");
+		text[9].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 2));
+
+		// Task 10
+		text[10].setFont(font);
+		text[10].setFillColor(sf::Color::White);
+		text[10].setString("In-Degree Distribution*");
+		text[10].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 3));
+
+		// Task 11
+		text[11].setFont(font);
+		text[11].setFillColor(sf::Color::White);
+		text[11].setString("Out-Degree Distribution*");
+		text[11].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 4));
+
+		// Task 12
+		text[12].setFont(font);
+		text[12].setFillColor(sf::Color::White);
+		text[12].setString("Size of Largest SCC");
+		text[12].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 5));
+
+		// Task 13
+		text[13].setFont(font);
+		text[13].setFillColor(sf::Color::White);
+		text[13].setString("SCC Distribution*");
+		text[13].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 6));
+
+		// Task 14
+		text[14].setFont(font);
+		text[14].setFillColor(sf::Color::White);
+		text[14].setString("Size of Largest WCC");
+		text[14].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 7));
+
+		// Task 15
+		text[15].setFont(font);
+		text[15].setFillColor(sf::Color::White);
+		text[15].setString("WCC Distribution*");
+		text[15].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEMS + 1) * 8));
+
+		selectedItemIndex = 0;
 	}
+	~coolMenu() {};
 
 	void draw(sf::RenderWindow& window)
 	{
 		window.draw(background);
-		for (int count = 0; count < MAX_ITEMS; count++)
-			window.draw(text[count]);
+
+		for (int i = 0; i < MAX_ITEMS; i++)
+			window.draw(text[i]);
 	}
-	void moveUp()
+	void MoveUp()
 	{
-
+		if (selectedItemIndex - 1 >= 0)
+		{
+			text[selectedItemIndex].setFillColor(sf::Color::White);
+			selectedItemIndex--;
+			text[selectedItemIndex].setFillColor(sf::Color::Cyan);
+		}
 	}
-	void moveDown()
+	void MoveDown()
 	{
-
+		if (selectedItemIndex + 1 < MAX_ITEMS)
+		{
+			text[selectedItemIndex].setFillColor(sf::Color::White);
+			selectedItemIndex++;
+			text[selectedItemIndex].setFillColor(sf::Color::Cyan);
+		}
 	}
-
-private:
-	//int selectedItem;
-	sf::Font font;
-	sf::Text text[MAX_ITEMS];
-
-	sf::Texture texture;
-	sf::Sprite background;
+	int GetPressedItem() 
+	{ 
+		return selectedItemIndex; 
+	}
 };
--- Network Analysis using Graphs/Source.cpp
+++ Network Analysis using Graphs/Source.cpp
@@ -1,55 +1,85 @@
 #include "Header.h"
-
 #include<SFML/Graphics.hpp>
 #include "Menu.h"
-
-/*int main() {
+int main()
+{
 	string dataPath = "./Text.txt";
 	graph G1;
 	G1.createGraph(dataPath);
 
-	// Part 1
-	G1.numberOfNodes();
-	G1.numberOfEdges();
-	G1.numberOfSourceNodes();
-	G1.numberOfSinkNodes();
-	G1.numberOfIsolatedNodes();
-	G1.noOfArticulationNodes();
-
-	//G1.findShortestPath(3466, 10310);
-	//G1.findShortestPath(3466, 1854);
-	//G1.findShortestPath(3466, 495);
-	//G1.findShortestPath(3466, 17968);
-	//G1.findShortestPath(14845, 9450);
-	//G1.findAllShortestPaths();
-	G1.findShortestPath(25543, 26048);
-
-	// Part 2
-	//G1.displayInDegree();
-	//G1.displayOutDegree();
-
-	// Part 3
-	G1.findLargestSCC();
-	//G1.displaySCC();
-	cout << endl;
-
-	G1.findLargestWCC();
-}*/
-
-int main()
-{
 	sf::RenderWindow window(sf::VideoMode(960, 720), "Network Analysis Using Graphs");
 
-	CoolMenu menu(window.getSize().x, window.getSize().y);
+	coolMenu menu(window.getSize().x, window.getSize().y);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 
-		while (window.pollEvent(event));
+		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					menu.MoveUp();
+					break;
+
+				case sf::Keyboard::Down:
+					menu.MoveDown();
+					break;
+
+				case sf::Keyboard::Return:
+					switch (menu.GetPressedItem())
+					{
+					case 0:
+						break;
+					case 1:
+						G1.numberOfNodes();
+						break;
+					case 2:
+						G1.numberOfEdges();
+						break;
+					case 3:
+						G1.numberOfSourceNodes();
+						break;
+					case 4:
+						G1.numberOfSinkNodes();
+						break;
+					case 5:
+						G1.numberOfIsolatedNodes();
+						break;
+					case 6:
+						break;
+					case 7:
+						G1.noOfArticulationNodes();
+						break;
+					case 8:
+						break;
+					case 9:
+						break;
+					case 10:
+						G1.displayInDegree();
+						break;
+					case 11:
+						G1.displayOutDegree();
+						break;
+					case 12:
+						G1.findLargestSCC();
+						break;
+					case 13:
+						break;
+					case 14:
+						G1.findLargestWCC();
+						break;
+					case 15:
+						break;
+					}
+					break;
+				}
+				break;
+
 			case sf::Event::Closed:
 				window.close();
 				break;
@@ -57,8 +87,9 @@ int main()
 		}
 
 		window.clear();
+
 		menu.draw(window);
+
 		window.display();
 	}
-	return 0;
 }
